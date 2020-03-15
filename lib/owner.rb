24089c89ee7dbb6f6f58048f6735b9a5e643a1b5
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 01:08:29 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
-  # code goes here
+
+  
+  attr_reader :name, :species
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+  
+  def say_species
+    puts "I am a #{self.species}."
+  end
+  
 end
