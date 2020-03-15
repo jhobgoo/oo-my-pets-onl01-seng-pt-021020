858c22b40282c1e2f4a403c6af55ee976cbceeaa
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 01:25:32 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,9 @@
 class Cat
 
-  attr_accessor :name, :owner
-
-  def initialize(owner)
+  attr_accessor :owner
+  attr_reader :name
+  
+  def initialize(name, owner)
     @name = name
     @owner = owner
   end
