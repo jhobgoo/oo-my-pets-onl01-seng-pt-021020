2abd7bfc5a050277deece32cf8468f6be29829a4
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 02:08:32 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,16 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
   end
-
+  
+  def self.all
+    @@all
+  end
+  
 end
