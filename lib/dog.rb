368ff5b9d5a6ce96c2270cce89641f25fed3a4ab
Joel Hobgood <jhobgoo@gmail.com>
2020-03-15 02:09:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+
+  attr_accessor: :name, :owner
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+  
 end
